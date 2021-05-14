bcb5111bc50778cc4769fad1890d5fe1ffc3c18e
Erich Kiss <erich.kiss2@gmail.com>
2021-05-14 12:34:04 +0200

    CRUD implemented

---
+++ src/Controller/EventsController.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Events;
+
+class EventsController extends AbstractController
+{
+    #[Route('/', name: 'events')]
+    public function index(): Response
+    {
+        $events = $this->getDoctrine()->getRepository('App:Events')->findAll();
+        // dd($events);
+
+        return $this->render('events/index.html.twig', array('events' => $events));
+    }
+
+    #[Route('/create', name: 'events_create')]
+    public function create(Request $request): Response
+    {
+        $event = new Events;
+
+        $form = $this->createFormBuilder($event)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('event_date', DateTimeType::class, array('attr' => array('style' => 'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('image', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('capacity', IntegerType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('email', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('phone', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('address', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('web_address', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('type', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+
+            ->add('save', SubmitType::class, array('label' => 'Create Event', 'attr' => array('class' => 'btn-primary', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $name = $form['name']->getData();
+            $event_date = $form['event_date']->getData();
+            $description = $form['description']->getData();
+            $image = $form['image']->getData();
+            $capacity = $form['capacity']->getData();
+            $email = $form['email']->getData();
+            $phone = $form['phone']->getData();
+            $address = $form['address']->getData();
+            $web_address = $form['web_address']->getData();
+            $type = $form['type']->getData();
+
+
+            $event->setName($name);
+            $event->setEventDate($event_date);
+            $event->setDescription($description);
+            $event->setImage($image);
+            $event->setCapacity($capacity);
+            $event->setEmail($email);
+            $event->setPhone($phone);
+            $event->setAddress($address);
+            $event->setWebAddress($web_address);
+            $event->setType($type);
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Event Added'
+            );
+
+            return $this->redirectToRoute('events');
+        }
+
+        return $this->render('events/create.html.twig', array('form' => $form->createView()));
+    }
+
+    #[Route('/edit/{id}', name: 'events_edit')]
+    public function edit(Request $request, $id): Response
+    {
+        $event = $this->getDoctrine()->getRepository('App:Events')->find($id);
+
+        $form = $this->createFormBuilder($event)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('event_date', DateTimeType::class, array('attr' => array('style' => 'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('image', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('capacity', IntegerType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('email', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('phone', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('address', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('web_address', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('type', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+
+            ->add('save', SubmitType::class, array('label' => 'Update Event', 'attr' => array('class' => 'btn-primary', 'style' => 'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $name = $form['name']->getData();
+            $event_date = $form['event_date']->getData();
+            $description = $form['description']->getData();
+            $image = $form['image']->getData();
+            $capacity = $form['capacity']->getData();
+            $email = $form['email']->getData();
+            $phone = $form['phone']->getData();
+            $address = $form['address']->getData();
+            $web_address = $form['web_address']->getData();
+            $type = $form['type']->getData();
+
+
+            $event->setName($name);
+            $event->setEventDate($event_date);
+            $event->setDescription($description);
+            $event->setImage($image);
+            $event->setCapacity($capacity);
+            $event->setEmail($email);
+            $event->setPhone($phone);
+            $event->setAddress($address);
+            $event->setWebAddress($web_address);
+            $event->setType($type);
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Event changed'
+            );
+
+            return $this->redirectToRoute('events');
+        }
+
+        return $this->render('events/edit.html.twig', array('event' => $event, 'form' => $form->createView()));
+    }
+
+    #[Route('/details/{id}', name: 'events_details')]
+    public function details($id): Response
+    {
+        $event = $this->getDoctrine()->getRepository('App:Events')->find($id);
+        return $this->render('events/details.html.twig', array('event' => $event));
+    }
+
+    #[Route('/delete/{id}', name: 'events_delete')]
+    public function delete($id){
+        $em = $this->getDoctrine()->getManager();
+        $event = $em->getRepository('App:Events')->find($id);
+        $em->remove($event);
+       
+
+        $em->flush();
+        $this->addFlash(
+            'notice',
+            'Event Removed'
+        );       
+
+        return $this->redirectToRoute('events');
+    }
+}
---
+++ src/Entity/Events.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EventsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EventsRepository::class)
+ */
+class Events
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $event_date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $capacity;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $web_address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEventDate(): ?\DateTimeInterface
+    {
+        return $this->event_date;
+    }
+
+    public function setEventDate(\DateTimeInterface $event_date): self
+    {
+        $this->event_date = $event_date;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCapacity(): ?int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity): self
+    {
+        $this->capacity = $capacity;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?int
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(int $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getWebAddress(): ?string
+    {
+        return $this->web_address;
+    }
+
+    public function setWebAddress(string $web_address): self
+    {
+        $this->web_address = $web_address;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
